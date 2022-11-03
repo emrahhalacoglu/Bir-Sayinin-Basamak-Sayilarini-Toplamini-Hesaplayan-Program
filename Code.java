89b01cb6d4c7a3fb6ba38abed119e28f15deece7
ehm <emrahhalacoglu@gmail.com>
2022-11-03 08:46:23 +0300

    Code file enriched

--- Code.java
+++ Code.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Code {
     public static void main(String[] args) {
         
+        //SAYININ BASAMAK RAKAMLARININ TOPLAMINI VEREN PROGRAM
+
         Scanner input=new Scanner(System.in);
-        
+  
         System.out.print("Input a Number : ");
         int n=input.nextInt();
         int tnd=n%10;
@@ -13,37 +15,66 @@ public class Code {
             tnd+=(n%10);
         }
         System.out.println("Total Number of Digits : "+tnd);
-    }
-    
-}
-
-/* Armstrong sayısı sorgulama örneği!
- * 
- * Scanner scanner = new Scanner(System.in);
-        System.out.print("n degerini giriniz: ");
-        
-        int n = scanner.nextInt();
+   
+        /*
+        //SAYININ ARMSTRONG OLUP OLMADIĞINI SORGULAYAN PROGRAM
+        System.out.print("Input a Number : ");
+        int n = input.nextInt();
         int count=0;
         int x=n*10;
         int armstrong=0;
+        int tempn=n;
         
-        while(n!=0){
-            n/=10; 
+        while(tempn!=0){
+            tempn/=10; 
             count++;
             }
-        System.out.println(count);
+        
         while(x!=0){
             x/=10; 
-            System.out.println(x);
             int b=x%10;
-            System.out.println(b);
             int y=b;
             for(int i=2;i<=count;i++){
                 y*=b;
-            
+            }
+            armstrong+=y;
+        }
+        if(armstrong==n){
+            System.out.println(n+" is an Armstrong Number.");
+        }else{
+            System.out.println(n+" is not an Armstrong Number.");
+        }
+   
+        
+        
+        *//*
+        //0-10000 ARASI BÜTÜN ARMSTRONG SAYILARI YAZDIRAN PROGRAM
+        
+        for(int i=1;i<10000;i++){
+            int tempi=i;
+            int count=0;
+            while(tempi!=0){
+                tempi/=10; 
+                count++;
             }
             
-            armstrong+=y;
-            System.out.println(armstrong);
- * 
- */
+            tempi=i;
+            int armstrong=0;
+            
+            while(tempi!=0){
+                int basvalue=tempi%10;
+                int baspow=1;
+                for(int j=1;j<=count;j++){
+                    baspow*=basvalue;
+                }
+                tempi/=10;
+                armstrong+=baspow;
+            }
+            
+            if(armstrong==i){
+            System.out.println(i+" is an Armstrong Number.");
+            }
+        }*/
+    }
+}
+
